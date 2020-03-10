9ab002edbe49d5e84064db633ddfc2cd4eab0d5c
Robert Palczewski <robert.palczewski@gmail.com>
2020-03-10 22:09:23 +0100

    Add changes for 27.4 contact form

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/contact/send-message', (req, res) => {
+  const { author, sender, title, message, image } = req.body;
+  
+  if (author && sender && title && message, image) {
+    res.render('contact', { isSent: true });
+  } else {
+    res.render('contact', { isError: true });
+  }
+});
 
 app.get('/', (req, res) => {
   res.render('index');
