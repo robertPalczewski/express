c6dab0c42b8c38ca368438145f8e2cbd6c730561
Robert Palczewski <robert.palczewski@gmail.com>
2020-03-10 21:43:58 +0100

    Add changes for 27.3 handlebars

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  res.show = name => {
-    res.sendFile(path.join(__dirname + `/views/${name}`));
-  };
-  next();
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
 
-app.get('/', (req, res) => {res.show('home.html');});
-app.get('/home', (req, res) => {res.show('home.html');});
-app.get('/about', (req, res) => {res.show('about.html');});
-app.use('/user', (req, res, next) => {res.show('forbidden.html');});
-app.get('/not-found.jpg', (req, res) => {res.sendFile(path.join(__dirname + '/not-found.jpg'));});
-app.use((req, res) => {res.status(404).show('404.html');});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
